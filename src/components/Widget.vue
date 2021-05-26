55b1eb788a51352a3acd7ba6c4309882906e1e03
Louis Allard <la.devweb@yahoo.com>
2021-05-26 10:42:58 +0200

    Integer twitter.

--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    Widget
-  </div>
+  <twitter>
+    <<a
+      class="twitter-timeline"
+      data-lang="fr"
+      data-width="300"
+      data-height="600"
+      href="https://twitter.com/lallard_59?ref_src=twsrc%5Etfw"
+      >Tweets by lallard_59</a
+    >
+  </twitter>
 </template>
 
 <script>
+import twitter from "vue-twitter";
 export default {
   name: "Widget",
   data() {
-    return {
-    };
-  },
+    return {};
+  }
 };
 </script>
