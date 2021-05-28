d92527249ba3bdb326de655878e571ae566c0f7b
Louis Allard <la.devweb@yahoo.com>
2021-05-28 17:20:42 +0200

    Add prices elements.

---
+++ src/components/Prices.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      Hello World
+  </div>
+</template>
+
+<script>
+//import { prices } from "../lng/fr";
+import "../sass/prices.scss";
+
+export default {
+  name: "Prices",
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+
+};
+</script>
