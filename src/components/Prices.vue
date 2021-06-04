d26e9ec6adc77664a3f84b2de827bed88b0cce84
Louis Allard <la.devweb@yahoo.com>
2021-06-04 11:04:40 +0200

    First prices elements.

--- src/components/Prices.vue
+++ src/components/Prices.vue
@@ -1,25 +1,70 @@
 <template>
-<b-container class="bv-example-row">
-  <b-row>
-    <b-col>Site vitrine</b-col>
-    <b-col>Site de vente</b-col>
-    <b-col>Offre complete</b-col>
-  </b-row>
-</b-container>
+  <b-container class="bv-example-row">
+    <b-row>
+      <b-col>&nbsp;</b-col>
+      <b-col>{{ prices.title01 }}</b-col>
+      <b-col>{{ prices.title02 }}</b-col>
+      <b-col>{{ prices.title03 }}</b-col>
+      <b-col>{{ prices.title04 }}</b-col>
+    </b-row>
+    <b-row>
+      <b-col>{{ prices.el01 }}</b-col>
+      <b-col>Y</b-col>
+      <b-col>Y</b-col>
+      <b-col>Y</b-col>
+      <b-col>Y</b-col>
+    </b-row>    
+    <b-row>
+      <b-col>{{ prices.el02 }}</b-col>
+      <b-col>N</b-col>
+      <b-col>Y</b-col>
+      <b-col>Y</b-col>
+      <b-col>Y</b-col>
+    </b-row>     
+    <b-row>
+      <b-col>{{ prices.el03 }}</b-col>
+      <b-col>N</b-col>
+      <b-col>N</b-col>
+      <b-col>Y</b-col>
+      <b-col>Y</b-col>
+    </b-row>
+    <b-row>
+      <b-col>{{ prices.el04 }}</b-col>
+      <b-col>N</b-col>
+      <b-col>N</b-col>
+      <b-col>Y</b-col>
+      <b-col>Y</b-col>
+    </b-row>        
+    <b-row>
+      <b-col>{{ prices.el05 }}</b-col>
+      <b-col>N</b-col>
+      <b-col>N</b-col>
+      <b-col>Y</b-col>
+      <b-col>Y</b-col>
+    </b-row>
+    <b-row>
+      <b-col>{{ prices.el06 }}</b-col>
+      <b-col>N</b-col>
+      <b-col>N</b-col>
+      <b-col>N</b-col>
+      <b-col>Y</b-col>
+    </b-row>       
+  </b-container>
 </template>
 
 <script>
-//import { prices } from "../lng/fr";
+import { prices } from "../lng/fr";
 import "../sass/prices.scss";
-
 export default {
   name: "Prices",
   data() {
     return {
+      prices,
     };
   },
   methods: {
   },
-
+  mounted() {
+  }
 };
 </script>
